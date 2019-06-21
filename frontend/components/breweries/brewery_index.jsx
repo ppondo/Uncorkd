82f6c1ab53f6cd0ea68ba20ee0663368457d1fec
ppondo <patrickpondo7@gmail.com>
2019-06-21 10:41:25 -0400

    add footer to brewery index page

--- frontend/components/breweries/brewery_index.jsx
+++ frontend/components/breweries/brewery_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BreweryIndexItem from './brewery_index_item';
+import { Link } from 'react-router-dom';
 
 
 class BreweryIndex extends React.Component {
@@ -19,15 +20,25 @@ class BreweryIndex extends React.Component {
         })
 
         return (
-            <div className='brewery-index-container'>
-                <div className='brewery-index'>
-                    <div className='brewery-index-count'>{this.props.breweries.length} Breweries</div>
-                    {BrewIndexItems}
+            <>
+                <div className='brewery-index-container'>
+                    <div className='brewery-index'>
+                        <div className='brewery-index-count'>{this.props.breweries.length} Breweries</div>
+                        {BrewIndexItems}
+                    </div>
+                    <div className='brewery-sidebar'>
+                        <div className='sidebar-title'>Global Top Beers</div>
+                    </div>
                 </div>
-                <div className='brewery-sidebar'>
-                    <div className='sidebar-title'>Global Top Beers</div>
+                <div className='home-footer'>
+                    <div className='home-footer-nav'>
+                        <a className='home-footer-link' href='https://github.com/ppondo/Uncorkd'>Github</a>
+                        <a className='home-footer-link' href='www.linkedin.com/in/patrickpondo' >LinkedIn</a>
+                        <a className='home-footer-link' href='' >Personal Site</a>
+                        <Link className='home-footer-link' to='/breweries' >Breweries</Link>
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
