018aa01d4fa03c5f87f80e01fdd603c30080e265
ppondo <patrickpondo7@gmail.com>
2019-06-24 17:25:45 -0400

    fix user profile bug

--- frontend/components/checkins/checkin_comment_item.jsx
+++ frontend/components/checkins/checkin_comment_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CheckinCommentItem = ({
     comment,
@@ -14,13 +15,19 @@ const CheckinCommentItem = ({
     if (commentUser === currentUser) {
         deleteButton = <div className='commentDelete'onClick={handleDelete}>Delete</div>
     }
-
+    debugger
     return (
         <div className='comment-item'>
             <img className='checkin-avatar-user' src={commentUser.imgUrl} alt="" />
-            <div className='comment-user'>{commentUser.username}:</div>
-            <div className='comment-body'>{comment.body}</div>
-            {deleteButton}
+            <div className='comment-right'>
+                <div className='comment-body'>
+                    <Link 
+                        to={`/users/${commentUser.id}`} 
+                        className='comment-user'>{commentUser.username}: </Link>
+                     { comment.body}
+                </div>
+                {deleteButton}
+            </div>
         </div>
     )
 }
